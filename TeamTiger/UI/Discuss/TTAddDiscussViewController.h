c4df41703f2678f81d0c0c6a53ef571f863a627d
CDJYuan <Dale@Dale-PC-2.local>
2016-08-04 19:17:56 +0800

    commit code

---
+++ TeamTiger/UI/Discuss/TTAddDiscussViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTAddDiscussViewController.h
+//  TeamTiger
+//
+//  Created by xxcao on 16/8/4.
+//  Copyright © 2016年 MobileArtisan. All rights reserved.
+//
+
+#import "TTBaseViewController.h"
+
+@interface TTAddDiscussViewController : TTBaseViewController
+
+@end
