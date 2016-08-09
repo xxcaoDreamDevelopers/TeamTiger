bd74557ee5e46d17d69ca8d365cda7a0d8409354
caoxingxing19881226 <xxcao@xxcaotekiMacBook-Pro-2.local>
2016-08-09 10:43:13 +0800

    commit codes

--- TeamTiger/UI/Setting/MyProfile/TTMyProfileViewController.m
+++ TeamTiger/UI/Setting/MyProfile/TTMyProfileViewController.m
@@ -59,7 +59,7 @@
 - (NSMutableArray *)dataSource {
     if (!_dataSource) {
         _dataSource = @[
-  @{@"Type":@0,@"Name":@"",@"Description":@"",@"ShowAccessory":@1,@"IsEdit":@0,@"Color":kRGB(21.0, 30.0, 44.0)},
+  @{@"Type":@0,@"Name":@"",@"Description":@"",@"ShowAccessory":@0,@"IsEdit":@0,@"Color":kRGB(21.0, 30.0, 44.0)},
   @{@"Type":@1,@"Name":@"名字",@"Description":@"唐晓旭",@"ShowAccessory":@1,@"IsEdit":@1,@"Color":kRGB(25.0, 34.0, 49.0)},
   @{@"Type":@1,@"Name":@"账号",@"Description":@"微信账号",@"ShowAccessory":@0,@"IsEdit":@0,@"Color":kRGB(26.0, 38.0, 55.0)},
   @{@"Type":@1,@"Name":@"新消息通知",@"Description":@"",@"ShowAccessory":@1,@"IsEdit":@0,@"Color":kRGB(31.0, 42.0, 63.0)},
